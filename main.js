a2a2423cb6769760f44aa8d6b2a4bdbe7f907451
MinhCaptain <centerisminh6@gmail.com>
2024-09-04 16:17:08 +0700

    project my

---
+++ main.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('#container');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const btnGrp = document.querySelector('btn-group');
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Anh cũng ưng cu!";
+    yesBtn.innerHTML = "Thật hả?" 
+
+    yesBtn.addEventListener('click' , () => {
+        question.innerHTML = "Anh nói đùa thôi!!";
+        noBtn.style.display = "none";
+        yesBtn.style.display ="none";
+    });
+});
+
+noBtn.addEventListener("mouseover", ()=>{
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const x = window.innerWidth - noBtnRect.width;
+    const y = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * x);
+    const randomY = Math.floor(Math.random() * y);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+})
